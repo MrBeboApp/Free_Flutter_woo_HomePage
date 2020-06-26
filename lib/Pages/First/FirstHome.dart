e18ea006bf14318180f7f9b85d5d9091e8ec987b
Bahaa Ehab El Dien Abdelmawla <mbcompany888@gmail.com>
2020-06-26 21:39:28 +0200

    Before the final touch of first home page

--- lib/Pages/First/FirstHome.dart
+++ lib/Pages/First/FirstHome.dart
@@ -8,14 +8,162 @@ class FirstHome extends StatefulWidget {
   _FirstHomeState createState() => _FirstHomeState();
 }
 
+Color _mainDarkColor = Color(0xff002638);
+
 class _FirstHomeState extends State<FirstHome> {
   @override
   Widget build(BuildContext context) {
     return Container(
        child: Scaffold(
-         body: Container(),
+         appBar: AppBar(
+           backgroundColor: Colors.transparent,
+           elevation: 0,
+           leading: IconButton(icon: Icon(Icons.add_location,color: _mainDarkColor,size: 30,), onPressed: (){
+           }),
+           actions: <Widget>[
+             Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: Column(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 crossAxisAlignment: CrossAxisAlignment.end,
+                 children: <Widget>[
+                   Text('Home Page',style: TextStyle(color:_mainDarkColor,fontSize: 15,fontWeight:FontWeight.bold),),
+                  Text('27 jun 2020',style: TextStyle(color:_mainDarkColor,fontSize: 12),),
+
+
+                 ],
+               ),
+             ),
+           ],
+         ),
+         body: Container(
+            child: ListView(
+              scrollDirection: Axis.vertical,
+             children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  //Edit the content of small Cart
+                  _smallCartInfo('Followers','56K',Icons.person,Colors.redAccent),
+                  _smallCartInfo('Comments','875',Icons.comment,Colors.blueAccent),
+                  _smallCartInfo('Following','576K',Icons.fiber_new,Colors.deepPurpleAccent),
+                  _smallCartInfo('Content','16K',Icons.contacts,Colors.deepPurple),
+
+                ],
+              ),
+              SizedBox(height: 20,),
+            
+              //Titel of the main section
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Main Content',style: TextStyle(color:_mainDarkColor,fontWeight: FontWeight.bold,fontSize: 20)),
+              ),
+
+              //Create the Main Card Content
+
+             _MainCard(),
+              
+             ],
+             
+           ),
+         ),
          bottomNavigationBar: NavigationTitled(),
        ),
     );
+
   }
+
+  //Create the Small cart Of Info
+
+  Widget _smallCartInfo (String title ,String subTitle , IconData myIcon ,Color myColor){
+
+    return Column(
+      children: <Widget>[
+        Container(
+
+                         height: 80,
+                         width: 80,
+                         decoration: BoxDecoration(
+                          color: myColor,
+                           borderRadius: BorderRadius.circular(10),
+ 
+
+                         ),
+                         child: Column(
+                           crossAxisAlignment: CrossAxisAlignment.center,
+                           mainAxisAlignment: MainAxisAlignment.center,
+                           children: <Widget>[
+                             Icon(myIcon,color: Colors.white,size: 22,),
+                                SizedBox(
+                               height: 4,
+                             ),
+                            
+                             Text(title,style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold,fontSize: 13),),
+                              SizedBox(
+                               height: 4,
+                             ),
+                              Text(subTitle,style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold,fontSize: 13),),
+                           ],
+                         ),
+                       ),
+
+                    
+      ],
+    );
+  }
+
+  Widget _MainCard(){
+
+    return    Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 500,
+                  child: Stack(
+                    children: <Widget>[
+                    Container(
+                        decoration: BoxDecoration(
+        
+                           color:Colors.white,
+                          image: const DecorationImage(
+                            image: NetworkImage('https://images.unsplash.com/photo-1562117906-7d850fcf80aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1184&q=80'),
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.circular(12),
+                      ), 
+                      ),
+                    Container(
+                      height: 500,
+                      color: Colors.white.withOpacity(0.5),
+    
+                    ),
+                    Center(child: Icon(Icons.play_circle_filled,color: Colors.red,size: 80,)),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                        Text('The Main title of content',style: TextStyle(color:_mainDarkColor,fontWeight: FontWeight.bold,fontSize: 15),),
+                         SizedBox(
+                                 height: 6,
+                               ),
+                        Text('Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+                        style: TextStyle(color:_mainDarkColor,fontSize: 14),),
+
+
+
+                        ],
+            
+                        ),
+                      )
+                    ],
+                  
+                  ),
+                ),
+              );
+
+  }
+
+
 }
