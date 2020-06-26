d5f2959f981169d652b49819061cc598ff89d06e
Bahaa Ehab El Dien Abdelmawla <mbcompany888@gmail.com>
2020-06-26 16:27:54 +0200

    Start First Home Page

---
+++ lib/Pages/First/FirstHome.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FirstHome extends StatefulWidget {
+  FirstHome({Key key}) : super(key: key);
+
+  @override
+  _FirstHomeState createState() => _FirstHomeState();
+}
+
+class _FirstHomeState extends State<FirstHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Container(),
+    );
+  }
+}
